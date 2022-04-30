83089f3856508de37fe86e018b8f03e0089345fb
Rafael Morales <rsoto@utem.cl>
2022-04-30 18:50:15 -0400

    Modelo separar nombres

---
+++ REST/API/src/main/java/com/redes/model/SeparadorNombresModel.java
@@ -0,0 +1,41 @@
+package com.redes.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SeparadorNombresModel implements Serializable {
+    private class Apellidos implements Serializable {
+        private String paterno, materno;
+
+        public Apellidos(String paterno, String materno) {
+            super();
+            this.paterno = paterno;
+            this.materno = materno;
+        }
+
+        public String getMaterno() {
+            return materno;
+        }
+
+        public String getPaterno() {
+            return paterno;
+        }
+    }
+
+    private List<String> nombres;
+    private Apellidos apellidos;
+
+    public SeparadorNombresModel(List<String> nombres, String paterno, String materno) {
+        super();
+        this.nombres = nombres;
+        this.apellidos = new Apellidos(paterno, materno);
+    }
+
+    public Apellidos getApellidos() {
+        return apellidos;
+    }
+
+    public List<String> getNombres() {
+        return nombres;
+    }
+}
